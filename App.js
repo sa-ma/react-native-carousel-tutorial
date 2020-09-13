8b085d9521c2d6e4638e57153d908f61748d3bc4
Samaila Bala <samailabalap@gmail.com>
2020-09-13 20:41:29 +0100

    add a react native carousel

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import CarouselCards from './CarouselCards'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <CarouselCards />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50
   },
 });
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# React Natve Carousel Tutorial
+
+This repository contains code for my tutorial on implementing a carousel in react native.
+
+## Requirements
+
+- Node v10+
+- npm and/or yarn
+- Expo
+
+## Installation Instructions
+
+1. Clone Repository
+2. Install dependencies with `yarn install`
+3. Start Expo with `yarn start`
